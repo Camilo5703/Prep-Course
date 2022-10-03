8b099f2dbd52338ace8f8947529ab74e44c45479
unknown <camiloh195703@hotmail.com>
2022-10-02 19:30:45 -0500

    secondhw

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -74,6 +74,11 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  function esDiezOCinco(numero) {
+    // Devuelve "true" si "numero" es 10 o 5
+    // De lo contrario, devuelve "false"
+    // Tu código:
+  return 10===numero||5===numero;
 }
 
 function estaEnRango(numero) {
