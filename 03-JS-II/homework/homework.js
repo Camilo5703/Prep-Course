4176f86505f3c3fabf82c6ff440fa2bf18e3700d
unknown <camiloh195703@hotmail.com>
2022-10-03 21:27:59 -0500

    final

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -200,13 +200,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  a=numero;
-  i=0;
-  do{
-    i=i++;
-    a=a+5;
+  var a = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    a = a + 5;
   }
-  while(i<8);return a;
+  while(i < 8);
+  return a;
 }
 
 
